abc4db8f5c9de9eccfc960be8387cf54d683b84e
Sam Work <Work@wifi-172-23-184-9.uoa-wifi.auckland.ac.nz>
2025-05-13 10:52:00 +1200

    Fix main file formatting and header comments

--- main.c
+++ main.c
@@ -1,44 +1,45 @@
 /*
- * LAB3part4.c
+ * Binary-Incrementer.c
  *
  * Created: 13/05/2025 9:51:25 AM
- * Author : jstr633
- */ 
+ * Authors : Jack Street, Sam Richell-Smith,
+ */
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 #define TIME_DELAY 50
 
-
 int main(void)
 {
-    /* Replace with your application code */
+	/* Replace with your application code */
 	DDRD = 0xFF;
 	DDRC = 0x00;
 	PORTD = 0x00;
-	
+
 	uint8_t curr = 0x00;
-	
-    while (1) 
-    {
-		if(PINC & (1 << PINC4))
+
+	while (1)
+	{
+		if (PINC & (1 << PINC4))
 		{
-			if (curr == 255){
+			if (curr == 255)
+			{
 				curr = 254;
 			}
 			curr++;
-		} 
-		else {
-			if (curr == 0){
+		}
+		else
+		{
+			if (curr == 0)
+			{
 				curr = 1;
 			}
-			curr--;	
+			curr--;
 		}
-		
+
 		PORTD = curr;
-		
+
 		_delay_ms(TIME_DELAY);
 	}
 }
-
